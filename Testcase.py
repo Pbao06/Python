36df2c630ee2e30b03ee8154bb7ced422273d46b
JOHNNY PHAN <phangia223@gmail.com>
2026-04-17 08:35:34 +0700

    Phan Gia Bao đẩy code lần đầu

---
+++ Testcase.py
@@ -0,0 +1,14 @@
+# Ask user for their name
+name = input("Enter your name: ")
+
+# Ask user for a number
+number = int(input("Enter a number: "))
+
+# Greet the user
+print("Hello,", name)
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print("Your number is even")
+else:
+    print("Your number is odd")
